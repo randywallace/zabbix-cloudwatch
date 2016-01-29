9a10d3f9da6daf9ebec4e0b06c0c5b68cba0a057
Randy D. Wallace Jr <randy@randywallace.com>
2016-01-29 04:57:36 -0500

    Update zabbix-cloudwatch.rb

--- lib/zabbix-cloudwatch.rb
+++ lib/zabbix-cloudwatch.rb
@@ -21,7 +21,7 @@ module ZabbixCloudwatch
       raise MetricnameArgumentMissingException unless options.key?"metricname"
       raise DimensionArgumentMissingException unless options.key?"dimension-name"
       raise DimensionArgumentMissingException unless options.key?"dimension-value"
-      self.aws = AWS::CloudWatch.new(get_aws_options).client
+      self.aws = Aws::CloudWatch.new(get_aws_options).client
     end
 
     def get_aws_options
